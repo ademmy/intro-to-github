99013ab7f8289822ddb2d4bfc68be9596d469c2e
ademmy <adebayotosin101@gmail.com>
2018-11-03 07:40:12 -0700

    first commmit

---
+++ github_intro.py
@@ -0,0 +1,2 @@
+print('this has nothing to do with you, \n i am just introducing myself to the github world')
+print('my name is Adebayo Oluwatosin and i am a data scientist \n expect great content from me \n cheers!')
